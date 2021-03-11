3550e936200552d607aa9a0110d5fdb89a3ac2ae
Oihana Coulaud <oihana.coulaud@outlook.fr>
2021-03-11 11:23:01 +0100

    Add PHP config, Make Pin entity timestampable, Listing on index by createdAt desc

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass=PinRepository::class)
  * @ORM\Table(name="pins")
+ * @ORM\HasLifecycleCallbacks
  */
 class Pin
 {
@@ -27,6 +28,16 @@ class Pin
      * @ORM\Column(type="text")
      */
     private $description;
+
+    /**
+     * @ORM\Column(type="datetime", options ={"default": "CURRENT_TIMESTAMP"})
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", options ={"default": "CURRENT_TIMESTAMP"})
+     */
+    private $updateAt;
 
     public function getId(): ?int
     {
@@ -55,5 +66,43 @@ class Pin
         $this->description = $description;
 
         return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdateAt(): ?\DateTimeInterface
+    {
+        return $this->updateAt;
+    }
+
+    public function setUpdateAt(\DateTimeInterface $updateAt): self
+    {
+        $this->updateAt = $updateAt;
+
+        return $this;
+    }
+
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function updateTimestamps()
+    {
+        if ($this->getCreatedAt() === null)
+        {
+            $this->setCreatedAt(new \DateTimeImmutable);
+        }
+        
+        $this->setUpdateAt(new \DateTimeImmutable);
     }
 }
