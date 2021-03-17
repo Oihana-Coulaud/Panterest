c7d0d1843734c40e02728b421c1b61db1aed9f06
Oihana Coulaud <oihana.coulaud@outlook.fr>
2021-03-17 15:32:31 +0100

    modify Entity/Pin and migrations (bug)

--- migrations/Version20210317142344.php
+++ migrations/Version20210317142344.php
@@ -20,12 +20,12 @@ final class Version20210311094800 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE pins CHANGE created_at created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, CHANGE update_at update_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
+        $this->addSql('ALTER TABLE pins ADD created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, ADD  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE pins CHANGE created_at created_at DATETIME NOT NULL, CHANGE update_at update_at DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE pins DROP created_at DATETIME NOT NULL, DROP updated_at DATETIME NOT NULL');
     }
 }
--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\PinRepository;
 use Doctrine\ORM\Mapping as ORM;
 use App\Entity\Traits\Timestampable;
+use DateTimeImmutable;
 
 /**
  * @ORM\Entity(repositoryClass=PinRepository::class)
@@ -30,6 +31,17 @@ class Pin
      * @ORM\Column(type="text")
      */
     private $description;
+
+     /**
+     * @ORM\Column(type="datetime", options ={"default": "CURRENT_TIMESTAMP"})
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", options ={"default": "CURRENT_TIMESTAMP"})
+     */
+    private $updatedAt;
+
 
     public function getId(): ?int
     {
@@ -58,6 +70,44 @@ class Pin
         $this->description = $description;
 
         return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function updateTimestamps()
+    {
+        if ($this->getCreatedAt() === null)
+        {
+            $this->setCreatedAt(new \DateTimeImmutable);
+        }
+
+        $this->setUpdatedAt(new \DateTimeImmutable);
     }
 
 }
